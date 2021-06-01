1c20eaaa982162686e76ca093c2826e73cffb9fb
SagorikaKubi <sagorika.tech@gmail.com>
2021-06-01 23:23:53 +0600

    initial setup done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// // import Header from './containers/Header';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// // import ProductListing from './containers/ProductListing';
+// // import ProductDetail from './containers/ProductDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
