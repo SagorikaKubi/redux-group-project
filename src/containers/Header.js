d317695d6ff1bb641f7308021aa16c002918b174
Muzammel Haque <mhktushar018@gmail.com>
2021-06-02 22:36:40 +0600

    updated by muzammel

---
+++ src/containers/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css'
+import img from "../image/downloadjsdfskdjfhsdjkfh.jpg"
+const Header = () => {
+    return (
+        <div>
+            <h1>THE WORLD</h1>
+        </div>
+    );
+};
+
+export default Header;
